d9b26b05d95dd59753000b74931059ea88536b9b
ankitkumar04100 <ankitkumarforall@gmail.com>
2026-02-06 20:21:22 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+import Assessment from "./pages/Assessment";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Assessment />
+    </div>
+  );
+}
+
+export default App;
